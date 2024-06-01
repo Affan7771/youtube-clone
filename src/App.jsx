6cbeffee063b289b216a0e6579301f4e581cd805
Affan <qureshiaffan99@gmail.com>
2024-06-01 13:09:52 +0530

    Youtube

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Video from './pages/video/Video'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/video/:categoryId/:videoId' element={ <Video /> } />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./Navbar.css"
+import MenuIcon from "../../assets/menu.png"
+import Logo from "../../assets/logo.png"
+import searchIcon from "../../assets/search.png"
+import uploadIcon from "../../assets/upload.png"
+import moreIcon from "../../assets/more.png"
+import notificationIcon from "../../assets/notification.png"
+import profileIcon from "../../assets/jack.png"
+
+const Navbar = () => {
+    return (
+        <nav className="flex-div">
+            <div className="flex-div nav-left">
+                <img src={MenuIcon} className="menu-icon" alt="menu-icon" />
+                <img src={Logo} className="logo" alt="logo" />
+            </div>
+
+            <div className="nav-middle flex-div">
+                <div className="search-box flex-div">
+                    <input type="text" placeholder="Search..." name="" id="" />
+                    <img src={searchIcon} alt="search" />
+                </div>
+            </div>
+
+            <div className="nav-right flex-div">
+                <img src={uploadIcon} alt="" />
+                <img src={moreIcon} alt="" />
+                <img src={notificationIcon} alt="" />
+                <img src={profileIcon} className='user-icon' alt="" />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
